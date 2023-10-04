6b31d0b6e4d344e4b9b58313088c1cb3ae4bd8d1
tops-codes <togbonna256@gmail.com>
2023-10-04 10:51:23 +0100

    new folder for R scripts

---
+++ R/learning.R
@@ -0,0 +1,8 @@
+learning <-1
+df <- data.frame(
+  name=c('Olive', 'Pinochio', 'Queenie', 'Raphael'), 
+  gender=c('female', 'male', 'female', 'male'), 
+  height=c(1.7, 1.01, 1.3, 1.6),
+  weight=c(60, 30, 50, 40))
+print(df[1])
+#
